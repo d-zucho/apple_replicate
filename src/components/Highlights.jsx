172cf2ebd5b5fb154cbc233a8895f3e041360ae3
D_Zucho <danny.nico.dev@outlook.com>
2024-06-29 20:17:02 -0700

    add title and links to highlight. start carousel

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,8 +1,42 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { watchImg, rightImg } from '../utils';
+
 const Highlights = () => {
+  useGSAP(() => {
+    // title
+    gsap.to('#title', {
+      opacity: 1,
+      y: 0,
+    });
+
+    // links
+    gsap.to('.link', { opacity: 1, y: 0, duration: 1, stagger: 0.25 });
+  }, []);
+
   return (
-    <div>
-      <h1>Highlights</h1>
-    </div>
+    <section
+      id='highlights'
+      className='w-screen overflow-hidden h-full common-padding bg-zinc'
+    >
+      <div className='screen-max-width'>
+        <div className='mb-12 w-full md:flex items-end justify-between'>
+          <h1 id='title' className='section-heading'>
+            Get the highlights.
+          </h1>
+          <div className='flex flex-wrap items-end gap-5'>
+            <p className='link'>
+              Watch the film
+              <img src={watchImg} alt='film' className='ml-2' />
+            </p>
+            <p className='link'>
+              Watch the event
+              <img src={rightImg} alt='event' className='ml-2' />
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
