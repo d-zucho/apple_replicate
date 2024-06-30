eed2ac3ffb7b4be97e32f2ee0fcb2a568534ed34
D_Zucho <danny.nico.dev@outlook.com>
2024-06-29 20:06:30 -0700

    add hero videos and animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
     <section className='w-full nav-height bg-black relative'>
       <div className='h-5/6 w-full flex-center flex-col'>
         <p id='hero-title' className='hero-title'>
-          iPhone 15Pro
+          iPhone 15 Pro
         </p>
 
         <div className='md:w-10/12 w-9/12'>
